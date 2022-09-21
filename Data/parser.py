aa01559ad8ebeace1dc1d93afca455b8271ee0a4
BERLIN-HQ\mariam.riaz <mariam.riaz@pinguin.ag>
2022-09-21 17:42:26 +0200

    Adding papers

---
+++ Data/parser.py
@@ -0,0 +1,15 @@
+import json
+import os
+import requests
+directory = 'C:\\Users\\mariam.riaz\\PycharmProjects\\LongSumm\\extractive_summaries\\talksumm_summaries\\fullpapers'
+
+for filename in os.listdir(directory):
+    url = 'http://localhost:8080/v1'
+    headers = {'Content-type': 'application/pdf', 'Accept-Charset': 'UTF-8'}
+    data = open(filename, 'rb')
+    r = requests.post(url, data=data, headers=headers)
+    data2 = r.json()
+    paper = data2['title']
+    json_file = paper+'.json'
+    with open(paper+'.json', 'w') as json_file:
+        json.dump(data2, f)
