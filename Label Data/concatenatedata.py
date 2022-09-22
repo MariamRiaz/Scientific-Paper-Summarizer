84b51002264dacefb36359158e537b4686e274db
BERLIN-HQ\mariam.riaz <mariam.riaz@pinguin.ag>
2022-09-22 12:45:28 +0200

    Concatenatedata.py

---
+++ Label Data/concatenatedata.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import os
+import json
+
+cdir ='C:\\Users\\mariam.riaz\\PycharmProjects\\Scientific-Paper-Summarizer\\Label Data\\results'
+
+appended_data = []
+for subdir, dirs, files in os.walk(cdir):
+    for file in files:
+        f1 = os.path.join(subdir, file)
+        df = pd.read_csv(f1)
+        appended_data.append(df)
+
+(print("Appending finished"))
+appended_data = pd.concat(appended_data)
+(print("Concatination finished"))
+appended_data.to_csv('final.csv', encoding="utf-8", index=None)
+(print("finished"))
